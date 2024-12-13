394bdb87a7a7b7927ca4887f47ef8de447465186
michelle-currier <mushel@gmail.com>
2024-12-13 15:36:44 -0600

    minor tweeks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ export default function Home() {
   return (
     <main className="p-24 text-center">
       <h1 className="text-5xl font-black">Home</h1>
+      <img
+        src="https://picsum.photos/200"
+        aria-label="lorem picsum"
+        className="transition duration-300 ease-in-out hover:shadow-lg hover:border hover:border-green-700 hover:rounded-md hover:saturate-200"
+      />
     </main>
   );
 }
